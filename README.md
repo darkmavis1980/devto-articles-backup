8fef6e6f9a904d63e3df56d6171b17e75c611901
Alessio Michelini <amichelini@hubspot.com>
2022-12-07 11:52:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dev.to Article fetcher
+
+This simple CLI will download the articles for a give account to your local computer.
+
+> **Note**
+> The articles must be published, draft articles will not be downloaded
+
+## Resources
+
+- [Forem API](https://developers.forem.com/api)
---
+++ fetcher.py
@@ -0,0 +1,64 @@
+import sys
+import requests
+import os
+from lib.conf import getConfig
+
+config = getConfig()
+foremApi = config.get('DEVTO', 'API')
+perPage = config.get('DEVTO', 'PER_PAGE')
+destFolder = config.get('LOCAL', 'DEST_FOLDER')
+fileFormat = config.get('LOCAL', 'FORMAT')
+
+class MissingIdException(Exception):
+    pass
+
+def addArticleMeta(article, file):
+    articleId = article["id"]
+    url = article["url"]
+    published_at = article["published_at"]
+    tags = article["tags"]
+
+    file.write("<!-- ID:{} -->\n".format(articleId))
+    file.write("<!-- url:{} -->\n".format(url))
+    file.write("<!-- published_at:{} -->\n".format(published_at))
+    file.write("<!-- tags:{} -->\n".format(tags))
+
+def fetchPost(articleId):
+    try:
+        if articleId is None and not articleId.isNumeric():
+            raise MissingIdException
+
+        articleUrl = "{}articles/{}".format(foremApi, articleId)
+        print(articleUrl)
+        response = requests.get(articleUrl)
+        article = response.json()
+        body = "body_markdown"
+        extension = "md"
+        if fileFormat != "markdown":
+            body = "body_html"
+            extension = "html"
+        if not os.path.exists(destFolder):
+            print("Createing missing folder {}".format(destFolder))
+            os.mkdir(destFolder)
+        with open("{}/{}.{}".format(destFolder, article["id"], extension), "w") as outFile:
+            addArticleMeta(article, outFile)
+            outFile.write("# {}\n\r".format(article['title']))
+            outFile.write(article[body])
+            outFile.close()
+    except MissingIdException:
+        print("ID is not passed")
+    except:
+        print("Cannot load article id {}".format(articleId))
+
+
+def fetchPosts():
+    username = sys.argv[1]
+    allArticlesUrl = "{}articles?username={}&page=1&per_page={}".format(foremApi, username, perPage)
+    response = requests.get(allArticlesUrl)
+    listArticles = response.json()
+    for article in listArticles:
+        # print(article['id'])
+        fetchPost(article['id'])
+
+if __name__ == "__main__":
+    fetchPosts()
